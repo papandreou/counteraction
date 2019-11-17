823e76098faa88da199b7ba3990847356b5836f7
Andreas Lind <andreaslindpetersen@gmail.com>
2019-11-17 22:03:53 +0100

    Support more ways of instantiating

--- test/index.js
+++ test/index.js
@@ -1,9 +1,73 @@
-const cssCounter = require('../lib/index');
+const counteraction = require('../lib/index');
 const expect = require('unexpected');
 
 describe('index', function() {
-  it('should create a counter renderer', function() {
-    const renderer = cssCounter('lower-roman');
+  it('should create a counter renderer by name', function() {
+    const renderer = counteraction('lower-roman');
     expect(renderer.render(12), 'to equal', 'xii');
+  });
+
+  it('should create a @counter-style renderer by an options object', function() {
+    const renderer = counteraction({
+      system: 'fixed',
+      symbols: ['A', 'B']
+    });
+    expect(renderer.render(1), 'to equal', 'A');
+    expect(renderer.render(2), 'to equal', 'B');
+  });
+
+  it('should create a @counter-style renderer by a system string and an options object', function() {
+    const renderer = counteraction('fixed', {
+      symbols: ['A', 'B']
+    });
+    expect(renderer.render(2), 'to equal', 'B');
+  });
+
+  it('should create a @counter-style renderer by an object of props and an options object', function() {
+    const renderer = counteraction(
+      {
+        system: 'fixed',
+        symbols: ['A', 'B']
+      },
+      {
+        fallback: 'disc'
+      }
+    );
+    expect(renderer.render(1), 'to equal', 'A');
+    expect(renderer.render(2), 'to equal', 'B');
+    expect(renderer.render(3), 'to equal', '•');
+  });
+
+  describe('supports', function() {
+    describe('when given a name', function() {
+      it('should return true for a supported list-style-type', function() {
+        expect(counteraction.supports('disc'), 'to be true');
+      });
+
+      it('should return true for a supported @counter-style system', function() {
+        expect(counteraction.supports('fixed'), 'to be true');
+      });
+
+      it('should return false for a unsupported type', function() {
+        expect(counteraction.supports('foo'), 'to be false');
+      });
+    });
+
+    describe('when given an object with a system property', function() {
+      it('should return true for a supported @counter-style system', function() {
+        expect(
+          counteraction.supports({ system: 'fixed', symbols: ['A'] }),
+          'to be true'
+        );
+      });
+
+      it('should return false for a supported @counter-style system', function() {
+        expect(counteraction.supports({ system: 'foo' }), 'to be false');
+      });
+    });
+
+    it('should return true for a unsupported type', function() {
+      expect(counteraction.supports('foo'), 'to be false');
+    });
   });
 });
